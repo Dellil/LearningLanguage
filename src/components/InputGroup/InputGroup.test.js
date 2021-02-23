ff96346cde1da3e19b6b05a0b9cf1aa7a303c339
Dellil <aksaksdm@naver.com>
2021-02-24 00:17:14 +0900

    Remove redundant InputGroup testcode

--- src/components/InputGroup/InputGroup.test.js
+++ src/components/InputGroup/InputGroup.test.js
@@ -11,15 +11,4 @@ describe('InputGroup Component', () => {
 
 		expect(rows).toHaveLength(2);
 	});
-
-	// TODO => 여러 인풋들이 만들어져있을 때, 인풋 생성버튼을 누르면 어디로 가야할까요?
-	// it('Should be generated New Line after Current InputRow', () => {
-	// 	const { getAllByRole, getByText, queryByText } = render(<InputGroup />);
-	// 	const createButton = getByText('새 줄');
-	// 	fireEvent.click(createButton);
-
-	// 	const firstCreateButton = queryByText('새 줄');
-	// 	fireEvent.click(firstCreateButton);
-
-	// });
 });
